da9364c5d0f030c586af5dcdf26dff5d2b1eb866
Rommel Cysne <bcysne@yahoo.com.br>
2022-07-31 10:35:27 -0300

    feat: Creating Search page

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { UserProfile } from 'types/vendor/github';
+import Button from '../../components/Button';
+import ResultCard from '../../components/ResultCard';
+
+import "./styles.css";
+
+type FormData = {
+  user: string;
+}
+
+const Search = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    user: '',
+  });
+
+  const [userProfile, setUserProfile] = useState<UserProfile>();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios
+    .get(`https://api.github.com/users/${formData.user}`)
+    .then((response) => {
+      setUserProfile(response.data);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      setUserProfile(undefined);
+      console.log(error);
+    });
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    console.log(value);
+
+    setFormData( {...formData, [name]:value} );
+  }
+
+  return (
+    <div className="search-container">
+      <form onSubmit={ handleSubmit }>
+        <div className="form-container">
+          <div className="base-card search-card">
+            <div>
+              <h1>Encontre um perfil Github</h1>
+              <input type="text" name="user" value={formData.user}
+                     placeholder="Usuário Github" onChange={ handleChange }/>
+            </div>
+
+            <div>
+              <Button text="Econtrar" />
+            </div>
+          </div>
+
+          { userProfile &&
+            <ResultCard avatar_url={userProfile.avatar_url} 
+                        url={userProfile.url} 
+                        followers={userProfile.followers} 
+                        location={userProfile.location} 
+                        name={userProfile.name}/>
+          }
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
